7cbdb920d1a1173b23bc3b0bfa8c15847cc85097
Marcin Bak <marcin.bak@neofonie.de>
2017-12-09 13:40:28 +0100

    Add some code and configure sonar

---
+++ app/src/main/java/de/neofonie/androidsonarkotlin/BusinessLogic.kt
@@ -0,0 +1,18 @@
+package de.neofonie.androidsonarkotlin
+
+/**
+ * Some business logic.
+ *
+ * Created by marcinbak on 09.12.17.
+ */
+class BusinessLogic {
+
+  fun someLogic(param1: String, param2: Int): String {
+    return "$param1:$param2"
+  }
+
+  fun moreLogic(x: Int, y: Int): Int {
+    return x + y
+  }
+
+}
--- app/src/main/java/de/neofonie/androidsonarkotlin/MainActivity.kt
+++ app/src/main/java/de/neofonie/androidsonarkotlin/MainActivity.kt
@@ -1,12 +1,32 @@
 package de.neofonie.androidsonarkotlin
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import java.util.concurrent.atomic.AtomicInteger
 
+/**
+ * Main activity class
+ */
 class MainActivity : AppCompatActivity() {
+
+  private lateinit var logic: BusinessLogic
+
+  private val counter: AtomicInteger = AtomicInteger(0)
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_main)
+    logic = BusinessLogic()
+  }
+
+  override fun onStart() {
+    super.onStart()
+    counter.incrementAndGet()
+    logic.someLogic(BuildConfig.VERSION_NAME, BuildConfig.VERSION_CODE)
+  }
+
+  override fun onStop() {
+    super.onStop()
+    logic.moreLogic(counter.get(), counter.decrementAndGet())
   }
 }
